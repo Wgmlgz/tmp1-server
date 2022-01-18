52d1570911003ca2d52f8f9e22fd502e81d140fc
Wgmlgz <62064707+Wgmlgz@users.noreply.github.com>
2022-01-18 21:49:33 +0300

    Product schema

---
+++ src/models/product.ts
@@ -0,0 +1,116 @@
+import mongoose from 'mongoose'
+
+export interface IProduct {
+  type?: string
+  category?: string
+  article?: string
+  name: string
+  description?: string
+  tags?: string[]
+  imgs?: string[]
+  imgs_big?: string[]
+  imgs_small?: string[]
+  videos?: string[]
+  buy_price: string
+  delivery_price: string
+  height: number
+  length: number
+  width: number
+  weight: number
+  brand?: string
+  count?: number
+  provider?: string
+  address?: string
+  user_creator_id?: string
+  changed?: Date
+  user_changed_id?: string
+}
+
+const ProductSchema = new mongoose.Schema<IProduct>({
+  type: {
+    type: String,
+    maxLength: 100,
+  },
+  category: {
+    type: String,
+  },
+  article: {
+    type: String,
+    maxLength: 100,
+  },
+  name: {
+    type: String,
+    required: true,
+    maxLength: 100,
+  },
+  description: {
+    type: String,
+    required: true,
+    maxLength: 2000,
+  },
+  tags: {
+    type: [String],
+  },
+  imgs: {
+    type: [String],
+  },
+  imgs_big: {
+    type: [String],
+  },
+  imgs_small: {
+    type: [String],
+  },
+  videos: {
+    type: [String],
+  },
+  buy_price: {
+    type: String,
+    required: true,
+  },
+  delivery_price: {
+    type: String,
+    required: true,
+  },
+  height: {
+    type: Number,
+    required: true,
+  },
+  length: {
+    type: Number,
+    required: true,
+  },
+  width: {
+    type: Number,
+    required: true,
+  },
+  weight: {
+    type: Number,
+    required: true,
+  },
+  brand: {
+    type: String,
+    maxLength: 100,
+  },
+  count: {
+    type: Number,
+  },
+  provider: {
+    type: String,
+    maxLength: 200,
+  },
+  address: {
+    type: String,
+    maxLength: 50,
+  },
+  user_creator_id: {
+    type: String,
+  },
+  changed: Date,
+  user_changed_id: {
+    type: String,
+  },
+})
+
+const ProductModel = mongoose.model('Product', ProductSchema)
+
+export default ProductModel
